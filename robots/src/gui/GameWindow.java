cce2357c135445aa7393aab6a4d87500d388492f
SRTsila <56469014+SRTsila@users.noreply.github.com>
2020-05-23 20:40:06 +0500

    Первое задание (#1)

--- robots/src/gui/GameWindow.java
+++ robots/src/gui/GameWindow.java
@@ -1,17 +1,12 @@
 package gui;
+import javax.swing.*;
+import java.awt.*;
 
-import java.awt.BorderLayout;
+public class GameWindow extends JInternalFrame {
 
-import javax.swing.JInternalFrame;
-import javax.swing.JPanel;
-
-public class GameWindow extends JInternalFrame
-{
-    private final GameVisualizer m_visualizer;
-    public GameWindow() 
-    {
+    public GameWindow() {
         super("Игровое поле", true, true, true, true);
-        m_visualizer = new GameVisualizer();
+        GameVisualizer m_visualizer = new GameVisualizer();
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(m_visualizer, BorderLayout.CENTER);
         getContentPane().add(panel);
